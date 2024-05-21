55479fa4388042ca0dac6334604f6903a9884067
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-21 19:23:19 -0300

    aprendendo tkinter

--- vision/main.py
+++ vision/main.py
@@ -33,11 +33,22 @@ buttonFrame = tk.Frame(root)
 buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
+buttonFrame.rowconfigure(0, weight=1)
+buttonFrame.rowconfigure(1, weight=1)
 
 btn1 = tk.Button(buttonFrame, text='1', font=('Arial', 18))
 btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
+btn2 = tk.Button(buttonFrame, text='2', font=('Arial', 18))
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
+btn3 = tk.Button(buttonFrame, text='3', font=('Arial', 18))
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+
+btn4 = tk.Button(buttonFrame, text='4', font=('Arial', 18))
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
+
+buttonFrame.pack()
 
 
 
