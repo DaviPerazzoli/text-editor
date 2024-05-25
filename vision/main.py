3167ede1a7bf47acd0e03b962ae321377d5c41a1
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-05-25 17:02:07 -0300

    trying to implement the scrollbars

--- vision/main.py
+++ vision/main.py
@@ -43,15 +43,35 @@ class Text_Editor:
         #Creating the main frame
         TEXT_BG_COLOR = '#FFF'
 
-        self.main_frame = tk.Frame(self.root,background=TEXT_BG_COLOR)
+        self.main_frame = tk.Frame(self.root, background=TEXT_BG_COLOR)
         self.main_frame.grid(row=1,column=1, columnspan=2, sticky='nsew')
+        self.main_frame.grid_rowconfigure(0, weight=1)
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.main_frame.grid_propagate(False)
+
+        #Creating a canvas to make the main frame scrollable
+
+        self.canvas = tk.Canvas(self.main_frame, background='blue')
+        self.canvas.grid(row=0, column=0, sticky="new")
+
+        self.vertical_scroll_bar = tk.Scrollbar(self.main_frame, orient=tk.VERTICAL, command = self.canvas.yview)
+        self.vertical_scroll_bar.grid(row=0, column=1, sticky='ns')
+        self.canvas.config(yscrollcommand = self.vertical_scroll_bar.set)
+
+        self.internal_frame = tk.Frame(self.canvas)
+        self.frame_id = self.canvas.create_window((0, 0), window=self.internal_frame, anchor='nw')
+
+        self.canvas.itemconfig(self.frame_id, width = self.canvas.winfo_width())
+
+        self.internal_frame.update_idletasks()
+
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
         #Creating the side frame for the line count
-        
-        self.side_frame = tk.Frame(self.main_frame, background=TEXT_BG_COLOR)
-        self.side_frame.grid(row=0, column=0, sticky='ns')
-        self.side_frame.columnconfigure(0, weight=1)
-        self.side_frame.rowconfigure(0, weight=1)
+        self.side_frame = tk.Frame(self.internal_frame, background=TEXT_BG_COLOR)
+        self.side_frame.grid(row=0, column=0, rowspan=2, sticky='ns')
+        self.side_frame.grid_columnconfigure(0, weight=1)
+        self.side_frame.grid_rowconfigure(0, weight=1)
 
         CODE_FONT = ('consolas', 11)
 
@@ -61,23 +81,24 @@ class Text_Editor:
         
         # Creating the textbox
         
-        self.textbox = tk.Text(self.main_frame, font=CODE_FONT, wrap='none')
+        self.textbox = tk.Text(self.internal_frame, font=CODE_FONT, wrap='none')
         self.textbox.grid(row=0,column=1, sticky='nsew')
         self.update_line_count()
+
+        self.root.update()
+        self.handle_resize()
+        self.root.bind("<Configure>", self.handle_resize)
 
         self.textbox.bind('<KeyPress>', self.handle_textbox_keypress)
 
         # root grid configuration
         self.root.columnconfigure(0,weight=0) # Explorer
         self.root.columnconfigure(1,weight=1) # Main frame
-        self.root.columnconfigure(2,weight=0) # Vertical scrollbar
         self.root.rowconfigure(1,weight=1) # Main frame
 
-        # main_frame grid configuration
-        self.main_frame.columnconfigure(0, weight=0)
-        self.main_frame.columnconfigure(1,weight=1)
-        self.main_frame.columnconfigure(2,weight=0)
-        self.main_frame.rowconfigure(1,weight=0)
+        # internal frame grid configuration
+        self.internal_frame.columnconfigure(1,weight=1)
+        self.internal_frame.rowconfigure(1,weight=0) # textbox scrollbar
         
         self.root.mainloop()
     
@@ -130,6 +151,11 @@ class Text_Editor:
             # The after_idle method calls the parameter function only after doing everything in the mainloop
             # It is being used to refresh the text widget before updating the line count
             self.textbox.after_idle(self.update_line_count)
+            self.textbox.configure(height=self.get_texbox_lines())
+    
+    def handle_resize(self, event=None):
+        self.main_frame.config(width=self.get_screen_width(),height=self.get_screen_height())
+        self.canvas.itemconfig(self.frame_id, width = self.get_screen_width())
 
     def test(self):
         print('test')
